b486bb54838b2bbf861137e78f989ee6dfd80c3f
luoxiaojun <luoxiaojun@dodoca.com>
2020-04-02 20:38:10 +0800

    txn

--- app/components/transaction/log/UndoLog.php
+++ app/components/transaction/log/UndoLog.php
@@ -4,5 +4,63 @@ namespace App\components\transaction\log;
 
 class UndoLog
 {
+    protected string $key;
 
+    protected string $val;
+
+    protected string $op;
+
+    /**
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     * @return $this
+     */
+    public function setKey(string $key): self
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVal(): string
+    {
+        return $this->val;
+    }
+
+    /**
+     * @param string $val
+     * @return $this
+     */
+    public function setVal(string $val): self
+    {
+        $this->val = $val;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOp(): string
+    {
+        return $this->op;
+    }
+
+    /**
+     * @param string $op
+     * @return $this
+     */
+    public function setOp(string $op): self
+    {
+        $this->op = $op;
+        return $this;
+    }
 }
