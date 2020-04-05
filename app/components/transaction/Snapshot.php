b36499cf98c99f415fe2868441c61325ba074aca
luoxiaojun <luoxiaojun@dodoca.com>
2020-04-05 16:52:42 +0800

    txn

--- app/components/transaction/Snapshot.php
+++ app/components/transaction/Snapshot.php
@@ -27,6 +27,16 @@ class Snapshot
     }
 
     /**
+     * @param array $idList
+     * @return $this
+     */
+    public function addIdList(array $idList): self
+    {
+        $this->idList = array_merge($this->idList, $idList);
+        return $this;
+    }
+
+    /**
      * @return array
      */
     public function getIdListGaps(): array
@@ -41,6 +51,16 @@ class Snapshot
     public function setIdListGaps(array $idListGaps): self
     {
         $this->idListGaps = $idListGaps;
+        return $this;
+    }
+
+    /**
+     * @param array $idListGaps
+     * @return $this
+     */
+    public function addIdListGaps(array $idListGaps): self
+    {
+        $this->idListGaps = array_merge($this->idListGaps, $idListGaps);
         return $this;
     }
 
