76de72842148f4e5918c18417469fe0ed0bd4574
luoxiaojun <luoxiaojun1992@sina.cn>
2020-11-21 18:18:16 +0800

    perf: add objects pool

--- config/pool.php
+++ config/pool.php
@@ -6,5 +6,11 @@ return [
         \SwFwLess\middlewares\Route::class => [
             'pool_size' => envInt('ROUTER_POOL_SIZE', 5),
         ],
+        \App\services\WriteService::class => [
+            'pool_size' => 5,
+        ],
+        \App\services\roydb\WriteService::class => [
+            'pool_size' => 5,
+        ],
     ],
 ];
